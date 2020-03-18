bcda92b06a2093cc74d96b4057e29e6791f640af
Iuliia Saprykina <iuliia.proskurnina@gmail.com>
2020-03-18 05:03:52 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -12,7 +12,9 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  crew_arr = []
+  crew.each {|crew_member| crew_arr << "Hello #{crew_member}."}
+  crew_arr
 end
 
 def engage
--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -1,17 +1,19 @@
-
+require 'pry'
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
 end
 
 def turtle_traits(turtles)
+  new_arr = []
   turtles.map do |turtle|
-    turtle[:traits].each do |trait|
-      trait
+    turtle[:traits].each do |val|
+        #binding.pry
+        new_arr << val
     end
   end
 end
